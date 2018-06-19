5a90a36efde409f92189d2df80757f63281b602b
ynte <contact@geoloep.nl>
2018-06-19 15:25:02 +0200

    fill index file

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import './components/autocomplete';
+import './components/dropdown';
+import './components/icon';
+import './components/loading';
+import './components/modal';
+import './components/tabs';
